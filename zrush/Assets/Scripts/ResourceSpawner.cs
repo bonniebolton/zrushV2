08319d7d3f8a5f9248609f1852f6025603370565
dearlydavid <47461572+dearlydavid@users.noreply.github.com>
2019-04-22 11:11:23 -0500

    finished resource spawn script
    moved zombo script to scripts folder also

--- zrush/Assets/Scripts/ResourceSpawner.cs
+++ zrush/Assets/Scripts/ResourceSpawner.cs
@@ -7,15 +7,15 @@ public class ResourceSpawner : MonoBehaviour
     // used to keep track of when the next spawn will occur
     private float nextTimeToSpawn = 0f;
     //float used to change spawnrate
-    public float ResourceSpawnRate = .125f;
+    public float ResourceSpawnRate = .1f;
 
     //used to reference the Level Location variable
     EnemySpawnManager eSpawn;
 
     //array storing spawn points
     public Transform[] Level1ResourceSpawnPoints;
-    //used to select spawn point
-    private int currentSpawnPoint = 0;
+    public Transform[] Level2ResourceSpawnPoints;
+    public Transform[] Level3ResourceSpawnPoints;
 
     private string[] ResourceDrops;
 
@@ -58,19 +58,28 @@ public class ResourceSpawner : MonoBehaviour
             
         }
 
-        //if the player is in level 1
+        //if the player is in level 2
         if (eSpawn.LevelLocation == 2)
         {
 
             //picks random spawn point
-            int i = Random.Range(0, Level1ResourceSpawnPoints.Length - 1);
+            int i = Random.Range(0, Level2ResourceSpawnPoints.Length - 1);
 
             //creates a zombie at the next level 1 spawn point
-            Instantiate(Resources.Load(PickRandomResource()), Level1ResourceSpawnPoints[i].position, Level1ResourceSpawnPoints[i].rotation);
+            Instantiate(Resources.Load(PickRandomResource()), Level2ResourceSpawnPoints[i].position, Level2ResourceSpawnPoints[i].rotation);
             
+        }
+
+        //if the player is in level 3
+        if (eSpawn.LevelLocation == 3)
+        {
+            //nothing spawns
+            return;
+
         }
     }
 
+    //picks a random resource from the array and returns a string
     public string PickRandomResource()
     {
         return ResourceDrops[Random.Range(0, ResourceDrops.Length - 1)];
